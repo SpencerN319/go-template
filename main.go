2553f5c7b92ab5f9618d604e04842c98e5748e1d
spencer nicol <spencern319@gmail.com>
2023-08-28 17:04:27 -0500

    zero logger.

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
-package main 
+package main
 
 import (
-	"fmt"
+	"github.com/SpencerN319/go-template/env"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
+func init() {
+	logLevel, _ := zerolog.ParseLevel(env.Getenv("LOG_LEVEL", "0"))
+	zerolog.SetGlobalLevel(logLevel)
+}
+
 func main() {
-	fmt.Println(hello())
+	log.Print(hello())
 }
